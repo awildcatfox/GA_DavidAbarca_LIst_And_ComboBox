909c4deeb6075e7462f4162e7be0c4974481068c
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2023-11-19 21:10:03 -0800

    finished assignment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,71 @@ namespace GA_DavidAbarca_LIst_And_ComboBox
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> firstNames = new List<string> { "John", "Jane", "Doe", "Mary", "Bob" };
+        List<string> lastNames = new List<string> { "Smith", "Doe", "Johnson", "Taylor", "Brown" };
+       
         public MainWindow()
         {
             InitializeComponent();
+            cmbFirstName.ItemsSource = firstNames; // Assign our list to the combo box
+            lbLastNames.ItemsSource = lastNames;
+        }
+
+        private void btnDisplayName_Click(object sender, RoutedEventArgs e)
+        {
+            // Getting selected index from combo box
+            int firstNameIndex = cmbFirstName.SelectedIndex;
+
+            // Getting selected index from list box
+            int lastNameIndex = lbLastNames.SelectedIndex;
+
+            // Using the index to get the first name from the list
+            string firstName = firstNames[firstNameIndex];
+
+            // Using the index to get the last name from the list
+            string lastName = lastNames[lastNameIndex];
+
+            // Concatenating a full name
+            string fullName = firstName + " " + lastName;
+
+            MessageBox.Show(fullName); // Display Full Name
+        }
+
+        private void btnAddNames_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = txtFirstName.Text; // Users first name input
+
+            string lastName = txtLastName.Text; // Users last name input
+
+            firstNames.Add(firstName); // Adding first name to first name list
+            lastNames.Add(lastName); // Adding last name to last name list
+
+            // Refresh each of the Boxes to show the newly added names to the list
+
+            cmbFirstName.Items.Refresh();
+            lbLastNames.Items.Refresh();
+        }
+
+        private void btnRemoveStudentAtIndex_Click(object sender, RoutedEventArgs e)
+        {
+            // Getting selected index from combo box
+            int firstNameIndex = cmbFirstName.SelectedIndex;
+            // Getting selected index from list box
+            int lastNameIndex = lbLastNames.SelectedIndex;
+
+            firstNames.RemoveAt(firstNameIndex); // Removing first name
+            lastNames.RemoveAt(lastNameIndex); // Removing last name
+
+
+            // Refresh each of the Boxes to show the newly added names to the list
+
+            cmbFirstName.Items.Refresh();
+            lbLastNames.Items.Refresh();
+        }
+
+        private void lbLastNames1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cmbFirstName.SelectedIndex = lbLastNames.SelectedIndex;
         }
     }
 }
